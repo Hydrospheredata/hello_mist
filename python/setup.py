2345c531434fdf222c5676b0250a5ab986cab902
dos65 <qtankle@gmail.com>
2018-05-24 11:53:01 +0300

    python example

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hello-mist',
-    version = '0.0.2',
+    version = '0.0.1',
     packages=find_packages(exclude=['tests']),
-    install_requires=["pyspark==2.3.0", "mistpy"]
+    install_requires=["pyspark==2.3.0", "mistpy==1.0.0rc16"]
 )
