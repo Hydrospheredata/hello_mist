7ebd1cb9c0ac5cfdd68382bad72c81fd58f80b6c
dos65 <qtankle@gmail.com>
2018-11-15 20:16:36 +0300

    1.0.0 -> 1.1.1

--- python/setup.py
+++ python/setup.py
@@ -5,5 +5,5 @@ setup(
     name='hello-mist',
     version = '0.0.1',
     packages=find_packages(exclude=['tests']),
-    install_requires=["pyspark==2.3.0", "mistpy==1.0.0"]
+    install_requires=["pyspark==2.3.0", "mistpy==1.1.1"]
 )
